fe75848fb1dd682a2f5443f76d911ec0e2869081
SungHoon(PC) <ksh920610@gmail.com>
2020-05-22 15:05:32 +0900

    프로젝트 생성

---
+++ Server/DummyClient/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * 임시 클라이언트 역할.
+ * 
+ *  Copyright 2020. SungHoon all rights reserved.
+ */
+
+namespace DummyClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Server/Server/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * 
+ * 
+ *  Copyright 2020. SungHoon all rights reserved.
+ */
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
