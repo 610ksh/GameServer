903f044f5f968932ded28125e76a482f3161f537
SungHoon(PC) <ksh920610@gmail.com>
2020-05-27 19:41:04 +0900

    DeadLock 실습

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -13,6 +13,48 @@ using System.Threading.Tasks; // for Task
 
 namespace ServerCore
 {
+    class SessionManager
+    {
+        static object _lock = new object();
+
+        public static void TestSession()
+        {
+            lock(_lock)
+            {
+
+            }
+        }
+
+        public static void Test()
+        {
+            lock (_lock)
+            {
+                UserManager.TestUser();
+            }
+        }
+    }
+
+    class UserManager
+    {
+        static object _lock = new object();
+
+        public static void Test()
+        {
+            lock(_lock)
+            {
+                SessionManager.TestSession();
+            }
+        }
+
+        public static void TestUser()
+        {
+            lock (_lock)
+            {
+
+            }
+        }
+    }
+
 
     class Program
     {
@@ -21,22 +63,22 @@ namespace ServerCore
 
         static void Thread_1()
         {
-            for (int i = 0; i < 1000000; ++i)
+            for (int i = 0; i < 1000; ++i)
             {
                 lock(_obj)
                 {
-                    num++;
+                    SessionManager.Test();
                 }
             }
         }
 
         static void Thread_2()
         {
-            for (int i = 0; i < 1000000; ++i)
+            for (int i = 0; i < 1000; ++i)
             {
                 lock (_obj)
                 {
-                    num--;
+                    UserManager.Test();
                 }
             }
         }
