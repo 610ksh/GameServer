a9548a00fdc49a5e9f72a70dbdad11238ed372d6
SungHoon(PC) <ksh920610@gmail.com>
2020-06-02 15:30:11 +0900

    Add Listener class

--- Server/DummyClient/Program.cs
+++ Server/DummyClient/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net; // for Dns
 using System.Net.Sockets; // for Socket
 using System.Text; // for Encoding
+using System.Threading; // for Thread
 using System.Threading.Tasks;
 
 /*
@@ -26,38 +27,44 @@ namespace DummyClient
             IPAddress ipAddr = iPHost.AddressList[0]; // 첫번째 IP주소를 가져옴(어차피 1개라서 그걸 가져오는거임)
             IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
 
-            // 휴대폰 설정
-            /// TCP는 stream과 Tcp가 세트처럼 묶여서 사용된다. 외워도 될정도.
-            Socket socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-
-            try
+            while (true)
             {
-                // 문지기한테 입장 문의
-                // 고객이 들고 있던 휴대폰으로 문지기한테 연락함. (입장가능한지 물어봄)
-                socket.Connect(endPoint); // 상대방 주소를 넣어줌. 입장 가능하면 아래 진행, 아니면 대기.
-                                          // 참고로 MMO에서는 이런 블로킹이 위험함. 문제점 : 만약 Connect를 했는데, 서버가 못받으면 상당히 오랜시간 여기서 대기를 타기 때문.
+                // 휴대폰 설정
+                /// TCP는 stream과 Tcp가 세트처럼 묶여서 사용된다. 외워도 될정도.
+                Socket socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
 
-                Console.WriteLine($"Connected To {socket.RemoteEndPoint.ToString()}");
+                try
+                {
+                    // 문지기한테 입장 문의
+                    // 고객이 들고 있던 휴대폰으로 문지기한테 연락함. (입장가능한지 물어봄)
+                    socket.Connect(endPoint); // 상대방 주소를 넣어줌. 입장 가능하면 아래 진행, 아니면 대기.
+                                              // 참고로 MMO에서는 이런 블로킹이 위험함
+                                              // 문제점 : 만약 Connect를 했는데, 서버가 못받으면 상당히 오랜시간 여기서 대기를 타기 때문.
 
-                // 보낸다 (서버와 반대임)
-                byte[] sendBuff = Encoding.UTF8.GetBytes("Hello Server, I'm Client");
-                int sendBytes = socket.Send(sendBuff); // 문제점 : 만약 서버에서 안받으면 좀 오래 대기하게 된다.
+                    Console.WriteLine($"Connected To {socket.RemoteEndPoint.ToString()}");
 
-                // 받는다
-                byte[] recvBuff = new Byte[1024];
-                int recvBytes = socket.Receive(recvBuff); // 몇바이트를 받는지 뱉어줌 // 문제점 : 만약 서버에서 아무 데이터도 안보내주면 좀 오래 대기하게 된다.
-                string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes); // 서버의 내용 string으로 해석
-                Console.WriteLine($"[From Server] {recvData}"); // 서버가 보낸것을 출력함
+                    // 보낸다 (서버와 반대임)
+                    byte[] sendBuff = Encoding.UTF8.GetBytes("Hello Server, I'm Client");
+                    int sendBytes = socket.Send(sendBuff); // 문제점 : 만약 서버에서 안받으면 좀 오래 대기하게 된다.
 
-                // 나간다.
-                socket.Shutdown(SocketShutdown.Both); //
-                socket.Close();
+                    // 받는다
+                    byte[] recvBuff = new Byte[1024];
+                    int recvBytes = socket.Receive(recvBuff); // 몇바이트를 받는지 뱉어줌 // 문제점 : 만약 서버에서 아무 데이터도 안보내주면 좀 오래 대기하게 된다.
+                    string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes); // 서버의 내용 string으로 해석
+                    Console.WriteLine($"[From Server] {recvData}"); // 서버가 보낸것을 출력함
+
+                    // 나간다.
+                    socket.Shutdown(SocketShutdown.Both); //
+                    socket.Close();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+
+                Thread.Sleep(500); // 0.1초
             }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.ToString());
-            }
-
+            
         }
     }
 }
--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -15,6 +15,34 @@ namespace ServerCore
 {
     class Program
     {
+        static Listener _listener = new Listener();
+
+        static void OnAcceptHandler(Socket clientSocket)
+        {
+            try
+            {
+                // 받는다
+                byte[] recvBuff = new byte[1024]; // 간단하게 만들어보자. 몇개를 보낼지 모름.
+                int recvBytes = clientSocket.Receive(recvBuff); // 보내준 데이터는 recvBuff에 저장됨. 몇바이트를 받은지를 뱉어줌.
+                string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes); // 문자열 인코딩 규약. 나중에 설명할예정. 버퍼로 받은 내용을 string으로 반환해줌. 시작인덱스, 길이
+                Console.WriteLine($"[From Client] {recvData}"); // 클라가 보낸 문자르 출력 (일단 여기서는 문자열을 보낸다고 가정하고 간단히 코딩해보자. 실제 서버는 다르다)
+
+                // 보낸다
+                byte[] sendBuff = Encoding.UTF8.GetBytes("Hi Client, Welcome to MMORPG Server !");
+                clientSocket.Send(sendBuff); // 상대방이 안받으면 대기를 하게 됨.
+                                             // ★ Accept(), Receive(), Send() 와 같은 모든 입출력 계열의 함수들은 무조건 비동기(논블로킹)으로 채택해야함
+
+                // 쫓아낸다.
+                clientSocket.Shutdown(SocketShutdown.Both); // 듣기도, 말하기도 싫다.
+                clientSocket.Close(); // 실제로 세션을 닫음.
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+
         static void Main(string[] args)
         {
             // DNS (Domain Name System)
@@ -25,60 +53,18 @@ namespace ServerCore
             IPAddress ipAddr = iPHost.AddressList[0]; // 첫번째 IP주소를 가져옴(어차피 1개라서 그걸 가져오는거임)
 
             IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777); // endPoint : 최종 주소. 7777 : 포트(식당 위치,문의 번호), ipAddr : 식당주소
-            // 참고로 클라가 7777포트가 아닌곳으로 요청, 접근을 시도하면, 입장을 못함. 클라도 포트를 맞춰줘야함.
+                                                                // 참고로 클라가 7777포트가 아닌곳으로 요청, 접근을 시도하면, 입장을 못함. 클라도 포트를 맞춰줘야함.
 
+            // 문지기에게 명령함. 혹시 어떤 요청이 들어오면, 나에게 OnAcceptHandler라는 곳으로 알려줘
+            _listener.Init(endPoint,OnAcceptHandler);
+            Console.WriteLine("Listening. . .");
 
-            // 문지기 (문지기 휴대폰 만들기)
-            /*
-             *  // .AddressFamily : ip4를 쓸지 6를 쓸지 결정하는건데, Dns에서 자동으로 결정했기에 따라가면됨.
-             *  // TCP를 사용할지 UDP를 사용할지 결정 
-             *  // 우린 TCP를 사용할거, SocketType을 Stream으로 하고, Tcp로 프로토콜 설정해줌.
-             * */
-            Socket listenSocket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-
-
-            // 혹시 중간에 에러가 날수도 있으므로, 예외처리를 해두자.
-            try
+            // 손님을 한번만 받고 끝낼게 아니기 떄문에, 무한루프
+            while (true)
             {
-                // 문지기 교육 (식당 주소와 포트번호까지 기입)
-                listenSocket.Bind(endPoint);
-
-                // 영업 시작
-                /*
-                 * backlog : 최대 대기수 (동시 다발적으로 접근했을때, 제한수 결정)
-                 * */
-                listenSocket.Listen(10); // 라이브에서 보통 조절
-
-
-                // 손님을 한번만 받고 끝낼게 아니기 떄문에, 무한루프
-                while (true)
-                {
-                    Console.WriteLine("Listening. . .");
-
-                    // 손님을 입장시킨다. (※기초적으로 코딩중이라, 실제 코드와는 다르다. 원래 보통 블로킹함수를 사용함)
-                    // 여기는 논블로킹인데, 블로킹이면 모든 실행이 Accept()에서 멈추고, 손님이 실제로 들어오면 아래를 실행, 아니면 실행을 안함.
-                    // 여기서는 만약 Accept()가 성공하지 않으면, 계속 대기한다고 생각하면 됨. 무한루프니까.
-                    Socket clientSocket = listenSocket.Accept(); // 세션의 소켓을 뱉어줌.
-                                                                 // 입장한 손님(대리인)과 대화하고 싶으면 이 소켓을 이용해서 소통함.
-                    // 받는다
-                    byte[] recvBuff = new byte[1024]; // 간단하게 만들어보자. 몇개를 보낼지 모름.
-                    int recvBytes = clientSocket.Receive(recvBuff); // 보내준 데이터는 recvBuff에 저장됨. 몇바이트를 받은지를 뱉어줌.
-                    string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes); // 문자열 인코딩 규약. 나중에 설명할예정. 버퍼로 받은 내용을 string으로 반환해줌. 시작인덱스, 길이
-                    Console.WriteLine($"[From Client] {recvData}"); // 클라가 보낸 문자르 출력 (일단 여기서는 문자열을 보낸다고 가정하고 간단히 코딩해보자. 실제 서버는 다르다)
-
-                    // 보낸다
-                    byte[] sendBuff = Encoding.UTF8.GetBytes("Hi Client, Welcome to MMORPG Server !");
-                    clientSocket.Send(sendBuff); // 상대방이 안받으면 대기를 하게 됨.
-
-                    // 쫓아낸다.
-                    clientSocket.Shutdown(SocketShutdown.Both); // 듣기도, 말하기도 싫다.
-                    clientSocket.Close(); // 실제로 세션을 닫음.
-                }
+               // 프로그램 종료되지 않게만 넣어주자.
             }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.ToString());
-            }
+
 
         }
     }
