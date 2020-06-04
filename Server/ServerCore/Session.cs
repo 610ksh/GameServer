c35792a0e16b417578743ee33dda9f6bcc058024
SungHooNi <ksh920610@gmail.com>
2020-06-04 19:57:22 +0900

    Modified BufferList of Session class (Session #3)

--- Server/ServerCore/Session.cs
+++ Server/ServerCore/Session.cs
@@ -14,22 +14,24 @@ namespace ServerCore
         int _disconnected = 0; // flag for Interlocked
 
         Queue<byte[]> _sendQueue = new Queue<byte[]>();
-        bool _pending = false;
 
         object _lock = new object(); // for lock
 
+        SocketAsyncEventArgs _recvArgs = new SocketAsyncEventArgs();
         SocketAsyncEventArgs _sendArgs = new SocketAsyncEventArgs(); // 재사용
+
+        List<ArraySegment<byte>> _pendingList = new List<ArraySegment<byte>>(); // for bufferList
 
         public void Start(Socket socket)
         {
             _socket = socket; // socket 연결
-            SocketAsyncEventArgs recvArgs = new SocketAsyncEventArgs();
-            recvArgs.Completed += new EventHandler<SocketAsyncEventArgs>(OnRecvCompleted);
-            recvArgs.SetBuffer(new byte[1024], 0, 1024); // 버퍼생성
+
+            _recvArgs.Completed += new EventHandler<SocketAsyncEventArgs>(OnRecvCompleted);
+            _recvArgs.SetBuffer(new byte[1024], 0, 1024); // 버퍼생성
 
             _sendArgs.Completed += new EventHandler<SocketAsyncEventArgs>(OnSendCompleted);
 
-            RegisterRecv(recvArgs); // 최초 실행
+            RegisterRecv(); // 최초 실행
         }
 
         public void Send(byte[] sendBuff)
@@ -38,7 +40,7 @@ namespace ServerCore
             {
                 // Critical Section
                 _sendQueue.Enqueue(sendBuff);
-                if (_pending == false)
+                if (_pendingList.Count == 0)
                     RegisterSend();
             }
         }
@@ -52,14 +54,18 @@ namespace ServerCore
             _socket.Close();
         }
 
-        #region 서버 통신
+        #region 네트워크 통신
 
-        // SendAsync()
+        //////// SendAsync
+
         void RegisterSend()
         {
-            _pending = true;
-            byte[] buff = _sendQueue.Dequeue(); // 버퍼 꺼내기
-            _sendArgs.SetBuffer(buff, 0, buff.Length); // 버퍼설정
+            while (_sendQueue.Count>0)
+            {
+                byte[] buff = _sendQueue.Dequeue(); // 버퍼 꺼내기
+                _pendingList.Add(new ArraySegment<byte>(buff, 0, buff.Length));
+            }
+            _sendArgs.BufferList = _pendingList;
 
             bool pending = _socket.SendAsync(_sendArgs);
             if (pending == false)
@@ -75,15 +81,17 @@ namespace ServerCore
                 {
                     try
                     {
+                        _sendArgs.BufferList = null; // 버퍼 초기화
+                        _pendingList.Clear(); // 버퍼리스트를 위한 초기화
+
+                        Console.WriteLine($"Transferred bytes : {_sendArgs.BytesTransferred}");
+
                         if (_sendQueue.Count > 0) // 큐가 빌때까지
                             RegisterSend(); // 비동기 실행 (낚싯대 던지기)
-                        else
-                            _pending = false; // 끝났음을 표시.
                     }
                     catch (Exception e)
                     {
                         Console.WriteLine($"OnSendCompleted is Failed {e}");
-                        throw;
                     }
                 }
                 else
@@ -93,25 +101,24 @@ namespace ServerCore
             }
         }
 
+        //////// ReceiveAsync
 
-        // 1.이벤트 등록
-        void RegisterRecv(SocketAsyncEventArgs args)
+        void RegisterRecv()
         {
-            bool pending = _socket.ReceiveAsync(args); // Receive를 하는건 socket임.
+            bool pending = _socket.ReceiveAsync(_recvArgs); // Receive를 하는건 socket임.
             if (pending == false)
-                OnRecvCompleted(null, args);
+                OnRecvCompleted(null, _recvArgs);
         }
 
-        // 2.이벤트 실행
         void OnRecvCompleted(object sender, SocketAsyncEventArgs args)
         {
             if (args.BytesTransferred > 0 && args.SocketError == SocketError.Success)
             {
                 try
                 {
-                    string recvData = Encoding.UTF8.GetString(args.Buffer, args.Offset, args.Count);
+                    string recvData = Encoding.UTF8.GetString(args.Buffer, args.Offset, args.BytesTransferred);
                     Console.WriteLine($"[From Client] {recvData}");
-                    RegisterRecv(args);
+                    RegisterRecv();
                 }
                 catch (Exception e)
                 {
