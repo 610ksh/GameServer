ae51d30830f43df62471dfe1cd047db545f89067
SungHooNi <ksh920610@gmail.com>
2020-06-19 17:42:09 +0900

    Applied Unity project #1

--- MMO_Server/Client/Assets/Scripts/Packet/PacketHandler.cs
+++ MMO_Server/Client/Assets/Scripts/Packet/PacketHandler.cs
@@ -3,6 +3,7 @@ using ServerCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using UnityEngine;
 
 class PacketHandler
 {
@@ -11,7 +12,7 @@ class PacketHandler
 		S_Chat chatPacket = packet as S_Chat;
 		ServerSession serverSession = session as ServerSession;
 
-		//if (chatPacket.playerId == 1)
-			//Console.WriteLine(chatPacket.chat);
-	}
+        if (chatPacket.playerId == 1)
+            Debug.Log(chatPacket.chat);
+    }
 }
